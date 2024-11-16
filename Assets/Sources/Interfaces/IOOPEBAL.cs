b4acf430ebf8cb1c747569b693f0c7490b8bcaa0
Big Boss <stulk7746@gmail.com>
2024-11-16 20:01:47 +0300

    save

---
+++ Assets/Sources/Interfaces/IOOPEBAL.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IOOPEBAL : Character
+{
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Move(Vector2 moveinput)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Stop()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TakeFix(int fixForce)
+    {
+        throw new System.NotImplementedException();
+    }
+}
